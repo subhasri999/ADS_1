d44e20e9edf14bdc256e08d80b01114897ccd17c
subhasri999 <43746092+subhasri999@users.noreply.github.com>
2018-10-05 13:02:01 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Module-3/3-sum/3-sum/Solution.java
+++ Module-3/3-sum/3-sum/Solution.java
@@ -1,35 +1,39 @@
-
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
 
-
-	
-	public static void main(String[] args) {
-		int count=0;
-		Scanner sc = new Scanner(System.in);
-		int n =sc.nextInt();
-		int [] array = new int[n];
-		
-		for(int i=0;i<array.length;i++) {
-			array[i]=sc.nextInt();
-			
-		}
-		
-		for(int i=0;i<array.length-2;i++) {
-			for(int j=i+1;j<array.length-1;j++) {
-				for(int k=j+1;k<array.length;k++) {
-					int a =array[i];
-					int b=array[j];
-					int c =array[k];
-					if(a+b+c==0) {
-						count++;
-					}
-				}
-			}
-		}
-		System.out.println(count);
-		
-	}
+	 public static int count(int[] arr) {
+	        int n = arr.length;
+	        Arrays.sort(arr);
+	        int count = 0;
+	        for (int i = 0; i < n; i++) {
+	            for (int j = i+1; j < n; j++) {
+	                int x = Arrays.binarySearch(arr, -(arr[i] + arr[j]));
+	                if (x > j) 
+	                {
+	                	count=count+1;
+	                }
+	                	
+	            }
+	        }
+	        return count;
+	    } 
+	 public static void main(String[] args)  
+	 { 
+		int n;
+	     Scanner scan = new Scanner (System.in);
+		n=scan.nextInt();
+	        int[] a = new int[n];
+	        for(int i=0;i<n;i++)
+	        {
+			a[i]=scan.nextInt();
+	        }
+	        int count = count(a);
+			//System.out.print("Number of Three sum number sets are:");
+	        System.out.println(count);
+	    } 
 }
+
+
+
