28dab92471b951da72b602595b39cf94dbcc82d0
subhasri999 <43746092+subhasri999@users.noreply.github.com>
2018-11-10 12:55:01 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- FinalPC/ADS_FinalExam/1/Solution.java
+++ FinalPC/ADS_FinalExam/1/Solution.java
@@ -1,96 +1,94 @@
 import java.util.Scanner;
-class Student
-{
-	String Name;
-	String Rollno;
-	double total_marks;
-	public Student(String name, String rollno, double total_marks) {
-		super();
-		Name = name;
-		Rollno = rollno;
-		this.total_marks = total_marks;
-	}
-	@Override
-	public String toString() {
-		return "Student [Name=" + Name + ", Rollno=" + Rollno + ", total_marks=" + total_marks + "]";
-	}
-	
-}
-class Hashing
-{
-	int n;
 
-	public Hashing(int m) {
-		this.n = m;
-	}
-	public int hashfunction(String st)
+class Node
+{
+	String roll_number;
+	double marks;
+	String StudentName;
+	Node(String s1,String s2,double d)
 	{
+		this.roll_number=s1;		
+		this.StudentName=s2;
+		this.marks=d;
+	}
+		 
+}
+ class Hash
+{
+	int k;
+
+	public Hash(int n) {
+		this.k=n;
+	}
+
+	public int hashfunction(String string) {
+		
+
 		int sum=0;
-		for(int i = st.length()-1;i>0;i--) 
+		for(int i = string.length()-1;i>0;i--) 
 			
 		{
-			sum=sum*17+(i+i+1)*st.charAt(i);
+			sum=sum*17+(i+i+1)*string.charAt(i);
 		}
-		sum=sum%n;
+		sum=sum%k;
 		
 		return sum;
 	}
 	
 }
 public class Solution {
-public static void main(String args[])
-{
-	Scanner s=new Scanner(System.in);
-	int n1=s.nextInt();
-Student student[]=new Student[n1*5];
-Hashing h=new Hashing(n1*5);
-String st=s.nextLine();
-String string[]=new String[n1];
-for(int i = 0;i<n1;i++)
-{
-	String s1=s.nextLine();
-	
-	String s2[]=s1.split(",");
-	int hc=h.hashfunction(s2[0]);
-	string[i]=s2[0];
-	Student stud=new Student(s2[1],s2[0],Double.parseDouble(s2[2]));
-	student[hc]=stud;	
-}
-
-int n2=s.nextInt();
-String x=s.next();
-int c=0;
-for(int i = 0;i<n2;i++)
-{
-	String s1=s.nextLine();
-	 c=0;
-	String s2[]=s1.split(" ");
-	for(int j = 0;j<string.length;j++)
+	public static void main(String args[])
 	{
-		if(s2[1].equals(string[j]))
+		Scanner sc= new Scanner(System.in);		
+		//System.out.println("No of Student Data Records");
+		int N=sc.nextInt();
+		Node Node[]=new Node[N*20];
+		Hash h=new Hash(N*20);
+		String st=sc.nextLine();
+		String string[]=new String[N];
+		for(int i = 0;i<N;i++)
 		{
-			c++;
-			break;
+			String s1=sc.nextLine();			
+			String s2[]=s1.split(",");
+			int hc=h.hashfunction(s2[0]);
+			string[i]=s2[0];
+			Node stud=new Node(s2[1],s2[0],Double.parseDouble(s2[2]));
+			Node[hc]=stud;	
+		}
+		int n2=sc.nextInt();
+		String x=sc.next();
+		int c=0;
+		for(int i = 0;i<n2;i++)
+		{
+			String s1=sc.nextLine();
+			 c=0;
+			String s2[]=s1.split(" ");
+			for(int j = 0;j<string.length;j++)
+			{
+				if(s2[1].equals(string[j]))
+				{
+					c++;
+					break;
+				}
+			}
+			if(c==1)
+			{
+			String s=sc.nextLine();
+			String[] k=s.split(" ");
+			switch (s2[2])
+			{
+			case "1":System.out.println(Node[h.hashfunction(s2[1])].StudentName);
+				break;
+			
+			case "2":
+				System.out.println(Node[h.hashfunction(s2[1])].marks);break;
+			}
+		}
+			else
+			{
+				System.out.println("Student doesn't exists...");
+			}
+		}
 		}
 	}
-	if(c==1)
-	{
-	
-	switch (s2[2])
-	{
-	case "1":System.out.println(student[h.hashfunction(s2[1])].Name);
-		break;
-	
-	case "2":
-		System.out.println(student[h.hashfunction(s2[1])].total_marks);break;
-	}
-}
-	else
-	{
-		System.out.println("Student doesn't exists...");
-	}
-}
 
-	
-}
-}
